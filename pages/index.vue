8c78b46afe7a79ce3bc08c23764ea600f71cd1cf
Matthieu Jacques <matthieu.jacques.dev@gmail.com>
2019-08-24 22:33:14 +0200

    init back end

--- pages/index.vue
+++ pages/index.vue
@@ -6,10 +6,14 @@
         helikia-app
       </h1>
       <h2 class="subtitle">
-        a beautiful project
+        A beautifull project to search EHPAD all around France
       </h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green"
+        >
           Documentation
         </a>
         <a
