e5942f206dd3c1038086fb8dcca634d64da560b1
Jacques Matthieu <matthieu.jacques.dev@gmail.com>
2019-08-27 09:37:12 +0200

    Revert "load fixture"
    This reverts commit baa067b42348821ba23c79bbb5b1e6ee58a0fea6.

--- packages/api/src/modules/helikia/helikia.plugin.mjs
+++ packages/api/src/modules/helikia/helikia.plugin.mjs
@@ -1,7 +1,8 @@
 export default {
-  name: 'helikia',
-  async register(server) {
-    server.plugins.mongodb.registerModel('User');
-    server.plugins.mongodb.registerModel('Establishement');
-  },
-};
+    name: 'helikia',
+    async register(server) {
+      server.plugins.mongodb.registerModel('User');
+      server.plugins.mongodb.registerModel('Establishement');
+    },
+  };
+  
